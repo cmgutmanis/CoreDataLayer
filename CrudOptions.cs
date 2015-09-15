f90d0ca49d9bff480235c9ded1ccc7d6851420ad
Chris Gutmanis <chris.gutmanis@gmail.com>
2015-09-14 23:01:13 -0500

    initial commit

---
+++ CrudOptions.cs
@@ -0,0 +1,55 @@
+﻿using reflectionmapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace CoreData
+{
+    public class FitPowerCrudOptions<TItem>: ICrudOptions<TItem>
+    {
+        private string connectionString { get; set; }
+        private SqlConnection connection { get; set; }
+        
+        public FitPowerCrudOptions(string connectionString)
+        {
+            this.connectionString = connectionString;
+            this.connection = new SqlConnection(connectionString);
+        }
+        
+        public TItem Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TItem> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(TItem item)
+        {
+            connection.Open();
+            var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            var mapped = PropertyMapper.GetParamsFromObject(item);
+            cmd.Parameters.Add(mapped);
+            cmd.CommandText = string.Concat("dbo.Add", typeof(TItem).ToString());
+
+            var id = Convert.ToInt32(cmd.ExecuteScalar());
+            return id;
+        }
+
+        public void Update(TItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ICrudOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreData
+{
+    public interface ICrudOptions<TItem>
+    {
+        TItem Get(int id);
+
+        IEnumerable<TItem> GetAll();
+
+        int Add(TItem item);
+
+        void Update(TItem item);
+
+        void Delete(int id);
+    }
+}
